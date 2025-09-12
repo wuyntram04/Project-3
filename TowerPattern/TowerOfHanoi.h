086e0524205ca5f9ebc4e6555eada37df2ed9f87
tim31-cmd <gthinhsc456@gmail.com>
2025-09-11 20:12:59 -0700

    Create TowerOfHanoi.h

---
+++ TowerPattern/TowerOfHanoi.h
@@ -0,0 +1,114 @@
+#include <stack>
+#pragma once
+using namespace std;
+
+class TowerOfHanoi
+{
+private:
+	int rings = 0;
+
+	stack <int> towerA;
+	stack <int> towerB;
+	stack <int> towerC;
+
+public:
+
+	void displayTower()
+	{
+		cout << "\n\t" << setw(rings + 1) << "\xBA" << "\n"; // ■ 
+
+		for (int i = 1; i <= rings; i++)
+		{
+			cout << "\t";
+			if (i <= towerA.size())
+			{
+				int space = 0;
+
+				while (space < (rings - i))
+				{
+					cout << " ";
+					space++;
+				}
+
+				for (int j = towerA.top(); j <= i; j++)
+				{
+					cout << "\xFE";
+				}
+
+				cout << i;
+
+				for (int j = towerA.top(); j <= i; j++)
+				{
+					cout << "\xFE";
+				}
+
+				cout << "\n";
+			}
+			else
+			{
+				cout << "\n\t" << setw(rings + 1) << "\xBA" << "\n"; // ■ 
+			}
+
+			cout << left << setw(10);
+
+			if (i <= towerB.size())
+			{
+				int space = 0;
+
+				while (space < (rings - i))
+				{
+					cout << " ";
+					space++;
+				}
+
+				for (int j = towerB.top(); j <= i; j++)
+				{
+					cout << "\xFE";
+				}
+
+				cout << i;
+
+				for (int j = towerB.top(); j <= i; j++)
+				{
+					cout << "\xFE";
+				}
+
+
+			}
+			else
+			{
+				cout << "\n\t" << setw(rings + 1) << "\xBA" << "\n"; // ■ 
+			}
+
+			cout << "\n";
+
+		}
+
+		cout << "\t";
+		for (int i = 0; i < rings; i++)
+		{
+			cout << "\xCD";
+		}
+
+		cout << "\xCA";
+
+		for (int i = 0; i < rings; i++)
+		{
+			cout << "\xCD";
+		}
+	}
+			
+	void setRings(int r)
+	{
+		rings = r;
+
+		for (int i = r; i > 0; --i)
+		{
+			towerA.push(i);
+		}
+	}
+
+	
+
+};
+
